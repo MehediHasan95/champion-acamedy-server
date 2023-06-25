78ec4c8b1ed9ab40a2296207b19bde360cadaeb7
MehediHasan95 <imehedihasan95@gmail.com>
2023-06-25 16:15:16 +0600

    mongodb connection complete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,11 +9,26 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const client = new MongoClient(
+  `mongodb+srv://${process.env.BUCKET_NAME}:${process.env.SECRET_KEY}@cluster0.mnvzcly.mongodb.net/?retryWrites=true&w=majority`,
+  {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  }
+);
+
 async function run() {
   try {
     app.get("/", (req, res) =>
       res.send("Champion Academy server run successfully")
     );
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("You successfully connected to MongoDB!");
+    const championDB = client.db("championAcademyBD");
   } finally {
     app.listen(port, () =>
       console.log("Champion Academy server is running successfully")
